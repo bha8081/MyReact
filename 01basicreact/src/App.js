d1d255ac33fc42fe633813ddc71c3bbc779cb86a
bha8081 <bhaveshy2000r@gmail.com>
2024-04-10 21:35:40 +0530

    Create react using Create react app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Pisea from "./money";
+
+function App() {
+  return (
+    <>
+        <h1>Welcom to React!!</h1>
+        <Pisea />
+        <p>Hi this is the react page by the using vite-react!!, I hope you like it.</p>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/money.js
@@ -0,0 +1,9 @@
+function Pisea() {
+    return (
+        <>
+            <h2>This is money page!!</h2>
+        </>
+    )
+}
+
+export default Pisea;
