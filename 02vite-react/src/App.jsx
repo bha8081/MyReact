50246dc1c5eedac2270eaac91555ed3003e5bc15
bha8081 <bhaveshy2000r@gmail.com>
2024-04-10 21:39:12 +0530

    Create react app using vite-react

---
+++ 02vite-react/src/App.jsx
@@ -0,0 +1,14 @@
+import Funy from "./Fun"
+
+function App() {
+
+  return (
+    <>
+        <h1>React with Vite!!!</h1>
+        <Funy />
+        <p>Hi budy i am your friend tell me now how to help you me, if you any need tell me i am try to help you.</p>
+    </>
+  )
+}
+
+export default App
---
+++ 02vite-react/src/Fun.jsx
@@ -0,0 +1,9 @@
+function Funy() {
+    return (
+        <>
+            <h2>This is fun function.</h2>
+        </>
+    )
+}
+
+export default Funy;
