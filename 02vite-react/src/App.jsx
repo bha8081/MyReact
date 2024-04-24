f6aaa7339eb61fdd58ff3616ee00d0e3b5694ce2
bha8081 <bhaveshy2000r@gmail.com>
2024-04-24 11:35:06 +0530

    Create git using vite

--- 02vite-react/src/App.jsx
+++ 02vite-react/src/App.jsx
@@ -1,10 +1,10 @@
 import Funy from "./Fun"
 
 function App() {
-
+  const username = "Virat"
   return (
     <>
-        <h1>React with Vite!!!</h1>
+        <h1>React with {username}!!!</h1>
         <Funy />
         <p>Hi budy i am your friend tell me now how to help you me, if you any need tell me i am try to help you.</p>
     </>
